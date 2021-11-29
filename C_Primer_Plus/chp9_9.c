e39fe257a098cb6dd13de12f7dcdcaeb9691d91a
qianjh <qianjhv@gmail.com>
2021-11-29 13:13:16 +0800

    chapter 9 finished

--- C_Primer_Plus/chp9_9.c
+++ C_Primer_Plus/chp9_9.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+void to_base_n(int, int);
+
 int main(void) {
 
-
+    for (int i=2; i<=16; i++) {
+        to_base_n(1888, i);
+        putchar('\n');
+    }
     return 0;
+}
+
+void to_base_n(int a, int n) {
+    if (a >= n) {
+        to_base_n(a / n, n);
+    }
+
+    if (a % n > 9) {
+        putchar(a % n + 55);
+    }else {
+        putchar((a % n) + '0');
+    }
 }
