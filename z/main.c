24cda4837c19aae28ae5a5846a63855af4b48589
qianjh <qianjhv@gmail.com>
2021-12-05 14:27:04 +0800

    reset main.c

--- z/main.c
+++ z/main.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
 
-    int *p1;
-    int *p3;
-    const int *p2;
-    const int **pp2;
-
-    printf("p1 = %p, &p3 = %p\n");
+    
 
     return 0;
 }
